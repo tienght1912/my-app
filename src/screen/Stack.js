c4bba08bc2de00137e1b1bb76046a9d043fca8eb
Tiếng HT <tieng1912@gmail.com>
2021-05-16 11:50:56 +0700

    ok

--- src/screen/Stack.js
+++ src/screen/Stack.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function Stack() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
     )
 }
 
