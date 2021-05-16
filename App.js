b75b9970b8876374eedaa283213dd400ce1f265a
Tiếng HT <tieng1912@gmail.com>
2021-05-16 13:35:56 +0700

    ok

--- App.js
+++ App.js
@@ -6,11 +6,8 @@ import Home from './src/screen/Home';
 import Settings from './src/screen/Settings';
 import Shopping from './src/screen/Shopping';
 import Favorite from './src/screen/Favorite';
-import {
-IconHome,
-IconShopping
-}
-  from 'react-native-vector-icons/Entypo';
+import IconHome from 'react-native-vector-icons/Entypo';
+import IconShopping from 'react-native-vector-icons/Entypo';
 import IconSettings from 'react-native-vector-icons/Ionicons';
 import IconFavo from 'react-native-vector-icons/MaterialIcons';
 
@@ -27,17 +24,37 @@ function MyTabs() {
       <Tab.Screen
         name="Home"
         options={{
-          tabBarIcon:({colorHome}) => (
-            <IconHome name="home" size={30} color={colorHome} />
-          )
+          tabBarIcon: ({color}) => (
+            <IconHome name="home" size={26} color={color} style={styles.iconTab} />
+          ),
+          title:""
         }}
         component={Home}/>
+        <Tab.Screen
+        name="Shopping"
+        options={{
+          tabBarIcon: ({color}) => (
+            <IconShopping name="shopping-cart" size={26} color={color} style={styles.iconTab} />
+          ),
+          title:""
+        }}
+        component={Shopping}/>
+        <Tab.Screen
+        name="Favorite"
+        options={{
+          tabBarIcon: ({color}) => (
+            <IconFavo name="favorite" size={26} color={color} style={styles.iconTab} />
+          ),
+          title:""
+        }}
+        component={Favorite}/>
       <Tab.Screen
         name="Settings"
         options={{
-          tabBarIcon: ({ colorSettings }) => (
-            <IconSettings name="settings" size={30} color={colorSettings} />
-          )
+          tabBarIcon: ({ color }) => (
+            <IconSettings name="settings" size={26} color={color} style={styles.iconTab} />
+          ),
+          title:""
         }}
         component={Settings} />
     </Tab.Navigator>
@@ -63,4 +80,8 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "#130f40"
   },
+  iconTab:{
+    justifyContent:"center",
+    marginTop:10
+  }
 })
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,36 +1,63 @@
 import React from 'react';
-import { 
-    StyleSheet, 
-    Text, 
-    View, 
-    Image, 
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
     FlatList,
     TextInput,
-    
+
 } from 'react-native';
+import IconNoti from 'react-native-vector-icons/Ionicons';
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <View style={styles.header}>
+                <Image/>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Search favorite food'}
+                    placeholderTextColor={'#778ca3'}
+                />
+                <IconNoti
+                    name="notifications" 
+                    size={22}
+                    color="#4b6584"
+                    style={styles.icon}
+                />
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        // flexDirection:"row",
         flex: 1,
-        backgroundColor: "#dff9fb",
+        backgroundColor: "#fff",
         // justifyContent: "center",
-        alignItems: "center",
+        // alignItems: "center",
     },
-    imgHome:{
-        width:80,
-        height:80,
+    header:{
+        flexDirection:"row",
+        width:"100%",
+        height:70,
+        // backgroundColor:"#a5b1c2",
+        justifyContent:"flex-end"
     },
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        color: "#130f40"
+    input:{
+        borderRadius:15,
+        backgroundColor:"#4b6584",
+        height:24,
+        fontSize:8,
+        padding:8,
+        width:150,
+        justifyContent:"flex-end",
+        marginTop:22
+    },
+    icon:{
+        marginTop:22
     },
 })
